5d034566f0180b6c28f35e6190000bb4d6a2be77
Rui Duarte <ruycruzeiro9@gmail.com>
2023-02-23 23:56:06 -0300

    first commit

---
+++ xml/server.js
@@ -0,0 +1,42 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+const multer = require('multer')
+
+
+app.use(express.static('.'))
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(bodyParser.json())
+
+app.get('/teste', (req, res) => {
+    res.send('ok')
+})
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err){
+            return res.send('ocorreu um erro.')
+        }
+
+        res.send('conclúido com sucesso.')
+    })
+})
+
+app.listen(8080, () => {
+    console.log('Server executando..')
+})
